5dd017eecbfd4aefb7ff28bc4cafa9458ea2ae44
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-06-03 00:02:21 +0530

    update the project

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,20 +1,47 @@
+import Axios  from "axios";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] =useState("");
+  const [password, setPassword] =useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    Axios.post('http://localhost:3000/auth/signin',{
+      email,
+      password
+    }).then(response => {
+      if(response.status === 200){
+        navigate("/home")
+      }
+    }).catch(err => console.log(err))
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form className="bg-white p-6 rounded shadow-md w-80">
-        <h2 className="text-2xl mb-4">Login</h2>
+    <div className="flex items-center justify-center h-screen bg-hero bg-cover">
+      <form className="bg-transparent border-2 border-gray-400 p-6 rounded shadow-md w-80" 
+      onSubmit={handleSubmit}>
+        <h2 className="text-2xl font-bold text-center mb-4 text-blue-200">Login</h2>
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="mb-6">
           <label className="block text-sm font-bold mb-2" htmlFor="password">Password</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" 
+          onChange={(e) => setPassword(e.target.value)}
+          type="password" placeholder="Password" />
         </div>
-        <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
+        <div className="flex items-center justify-center">
+          <button type="submit" className="text-white bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2 font-bold">Sign In</button>
         </div>
+        <div className="mt-3 border-t-2 border-blue-100 "></div>
+        <div className="flex justify-between text-blue-100 mt-3">
+          <p>Don't have account?</p>
+          <Link to="/signup" className="hover:text-blue-400">Sign Up</Link>
+          </div>
       </form>
     </div>
 
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,24 +1,72 @@
-// import bgImg from "
+import { useState } from "react";
+import Axios from "axios"
+import { Link, useNavigate } from "react-router-dom";
 export const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate()
+  const  handleSumbit =(e)=>{
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/signup",{name, email, password, confirmPassword})
+    .then((response) =>{
+      if(response.status === 200){
+        navigate('/login')
+      }
+    })
+    .catch((err) => console.log(err))
+
+  }
   return (
     <div className="flex items-center justify-center bg-hero bg-cover h-screen">
-      <form className="bg-transparent p-6 rounded shadow-md w-80">
-        <h2 className="text-2xl mb-4">Sign Up</h2>
+      <form className="bg-transparent border-2 border-gray-400 p-6 rounded shadow-md w-80" onSubmit={handleSumbit}>
+        <h2 className="text-2xl text-center font-bold mb-4 text-blue-100">Sign Up</h2>
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2" htmlFor="name">Name</label>
+          <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          id="name" 
+          type="text" 
+          placeholder="Name" 
+          onChange={(e) => setName(e.target.value)}/>
+        </div>
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+          <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          id="email" 
+          type="email" 
+          placeholder="Email" 
+          onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="password">Password</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+          <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          id="password" 
+          type="password" 
+          placeholder="Password" 
+          onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div className="mb-6">
           <label className="block text-sm font-bold mb-2" htmlFor="confirm-password">Confirm Password</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="confirm-password" type="password" placeholder="Confirm Password" />
+          <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          id="confirm-password" 
+          type="password" 
+          placeholder="Confirm Password" 
+          onChange={(e) => setConfirmPassword(e.target.value)}/>
         </div>
-        <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign Up</button>
+        <div className="flex items-center justify-center">
+          <button type="submit" className="text-white bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2 font-bold">Sign Up</button>
         </div>
+        <div className="mt-3 border-t-2 border-blue-100 "></div>
+        <div className="flex justify-between text-blue-100 mt-3">
+        
+          <p>Already have an Account?</p>
+          <Link to="/login" className="hover:text-blue-400">Login</Link>
+          </div>
       </form>
     </div>
   );
