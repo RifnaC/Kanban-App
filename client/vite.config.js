49eaef344a92f615ef1f229ef3155816940e9d7e
rifna <rifnazar2@gmail.com>
2024-07-18 10:07:38 +0530

    updated vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,27 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': 'react-router-dom'
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-});
+})
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       'react-router-dom': 'react-router-dom'
+//     }
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['react-router-dom']
+//     }
+//   }
+// });
 
